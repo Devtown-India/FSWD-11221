d5d164b6b16ee446ee4d6c3752026c6b0fb4805e
Kartik18g <gupta.kartik18kg@gmail.com>
2022-01-12 19:02:50 +0530

    operators

---
+++ assets/js/app.js
@@ -0,0 +1,66 @@
+/*
+
+ Assignment operators
+
+let age = 10
+
+
+console.log(++age)
+
+console.log(age)
+
+*/
+
+// comparison operator
+
+// let a = 10
+// let b = 15
+
+// console.log(a > b)
+// console.log(a < b)
+// console.log(a <= b)
+// console.log(a >= b)
+
+// let a = 10
+// let b = '10'
+
+
+// console.log(a == b)
+// console.log(a === b)
+
+// arithmetic operators
+
+// let a = 10
+// let b = 2
+
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a ** b)
+// console.log(a % b)
+// console.log(a / b)
+
+
+
+// let a = 10
+// let b = '10.1234'
+
+// console.log(Number(b) + a)
+
+// Logical operators
+
+// console.log('true && true', true && true)
+// console.log('true && false', true && false)
+// console.log('false && true', false && true)
+// console.log('false && false', false && false)
+
+
+
+
+// console.log('true || true', true || true)
+// console.log('true || false', true || false)
+// console.log('false || true', false || true)
+// console.log('false || false', false || false)
+
+
+
