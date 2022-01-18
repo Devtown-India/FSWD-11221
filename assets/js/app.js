5e54a9542c621119c4ce41755f6b001adfc194b5
Kartik18g <gupta.kartik18kg@gmail.com>
2022-01-18 18:49:54 +0530

    HTML collection vs Array

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,17 @@
-let x = 10
+// let elements = document.getElementsByClassName('temp')
+let elements = document.getElementsByClassName('div')
 
+let arr = [1, 2, 3, 4, 5]
+
+console.log(elements)
+console.log(arr)
+
+
+// for (let i = 0; i < elements.length; i++) {
+//     elements[i].innerHTML = `Element ${i}`
+
+// }
+
+elements.forEach((ele, i) => {
+    ele.innerHTML = `Element ${i}`
+});
