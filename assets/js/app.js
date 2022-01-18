f03127da4da4f553f35316ea1c8b58ba589f35e8
Kartik18g <gupta.kartik18kg@gmail.com>
2022-01-18 19:00:26 +0530

    changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
 // let elements = document.getElementsByClassName('temp')
-let elements = document.getElementsByClassName('div')
+// let elements = document.getElementsByTagName('div')
+let elements = document.querySelectorAll('div.temp')
 
 let arr = [1, 2, 3, 4, 5]
 
@@ -9,9 +10,8 @@ console.log(arr)
 
 // for (let i = 0; i < elements.length; i++) {
 //     elements[i].innerHTML = `Element ${i}`
-
 // }
 
 elements.forEach((ele, i) => {
-    ele.innerHTML = `Element ${i}`
+    ele.innerHTML = `Element ${i} ${i % 2 == 0 ? 'even' : "odd"}`
 });
