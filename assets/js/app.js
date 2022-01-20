9c0faa43797a57610a8a3fd065d56a82891020cf
Kartik18g <gupta.kartik18kg@gmail.com>
2022-01-20 19:23:29 +0530

    dark mode switch

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,44 +1,14 @@
 const button = document.querySelector('button')
-const input = document.querySelector('input#todo_input')
-const list = document.querySelector('ol')
+const div = document.querySelector('div')
+const body = document.querySelector('body')
 
 
-input.addEventListener('keydown', e => {
-    const key = e.key
-    if (key === 'Enter') {
-        // console.log("ENter key")
-        handleAdd()
-    }
+
+button.addEventListener('click', (e) => {
+    // check if the body has dark class
+    //  yes  -> remove it
+    //  no  -> add it
+    body.classList.toggle('dark')
 })
 
-
-const handleAdd = (e) => {
-
-    const textToAdd = input.value
-
-    if (textToAdd.length > 0) {
-        const ele = document.createElement('li')
-        const text_node = document.createTextNode(textToAdd)
-
-        ele.appendChild(text_node)
-        list.appendChild(ele)
-        // create a dynamic button element
-        const button = document.createElement('button')
-        button.innerHTML = "Delete"
-        ele.appendChild(button)
-
-        button.addEventListener('click', (e) => {
-            ele.remove()
-        })
-
-
-        // clear the list
-        input.value = ''
-    } else {
-        window.alert("Task can not be empty")
-    }
-
-}
-
-button.addEventListener('click', handleAdd)
 
