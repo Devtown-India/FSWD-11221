80e8115b1f9d248d602983c7b630bf45c4c45031
Kartik18g <gupta.kartik18kg@gmail.com>
2022-02-03 19:13:46 +0530

    config for class

---
+++ src/App.js
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga consequuntur sunt eum ipsa, nobis rerum et iure nam ea est, aspernatur, quibusdam unde molestiae sed tempora repellat iste eos cum!
+    </div>
+  );
+}
+
+export default App;
