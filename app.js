17022b4d4d9d0959d9dc85ad970a5d6a6ee50b05
Kartik18g <gupta.kartik18kg@gmail.com>
2022-03-08 19:23:27 +0530

    c

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
 
-const getData = ()=> new Promise(resolve=>{
-    setTimeout(()=>{
-        resolve({
-            brand:"Mercedes",
-            model:"GTR Black series"
-        })
-    },2000)
-})
+// const getData = ()=> new Promise(resolve=>{
+//     setTimeout(()=>{
+//         resolve({
+//             brand:"Mercedes",
+//             model:"GTR Black series"
+//         })
+//     },2000)
+// })
 
-const res = getData()
+// const res = getData()
 
-const callbackAfterPromise = (data)=>{
+
+// res
+// .then((data) => ({...data,engine:"V8 bitrubo"}))
+// .then(modifiedValue=>{
+//     console.log(modifiedValue)
+// })
+
+
+
+const response = fetch('https://jsonplaceholder.typicode.com/todos')
+
+// response.then(stream=>{
+//     console.log(stream)
+//     const dataPromise = stream.json()
+//     console.log(dataPromise)
+//     dataPromise.then(actualData=>{
+//         console.log(actualData)
+//     })
+// })
+
+response
+.then(stream => stream.json())
+.then(data=>{
     console.log(data)
-}
-
-res.then(callbackAfterPromise)
-
-// console.log(res)
-
-// setTimeout(()=>{
-//     console.log(res)
-
-// },2000)
+})
