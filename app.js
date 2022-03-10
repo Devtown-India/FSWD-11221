70ea424fceb2f65e14d8026943b54ea45dcf9b49
PriyaBihani <priyabihani.official@gmail.com>
2022-03-10 19:16:44 +0530

    cahnge

--- app.js
+++ app.js
@@ -1,22 +1,44 @@
+const app_id = "d4b775dd"
+const app_key = "734ddbad481c1d357d2806b62af85528"
 
-const container = document.querySelector("div.container")
+const input = document.querySelector("input")
+const button = document.querySelector("button")
+const results = document.querySelector('.results')
 
 
-const getData = async ()=>{
-    const stream = await fetch('https://jsonplaceholder.typicode.com/todos')
+const getRecipe = async (query="pizza")=>{
+
+    const endpoint = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${app_id}&app_key=${app_key}`
+    console.log(endpoint)
+
+    const stream =  await fetch(endpoint)
     const data = await stream.json()
 
-    console.log(data)
-    data.map(element=>{
-        const ele = document.createElement("h4")
-        ele.innerHTML= element.title
-        console.log(ele)
-        container.appendChild(ele)
+    data.hits.map((item)=>{
+        const recipe = item.recipe
+        const image = document.createElement('img')
+        image.setAttribute('src', recipe.image)
+        results.appendChild(image)
     })
+
+}
+
+const getRecipeWithDes = async (query="pizza")=>{
+
+    const endpoint = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${app_id}&app_key=${app_key}`
+
+    const stream =  await fetch(endpoint)
+    const {hits} = await stream.json()
+
+    hits.map(({recipe})=>{
+        const image = document.createElement('img')
+        image.setAttribute('src', recipe.image)
+        results.appendChild(image)
+    })
+
 }
 
 
-getData()
-
-
-
+button.addEventListener("click",(e)=>{
+    getRecipe(input.value)
+})
