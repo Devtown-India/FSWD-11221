a6fb7f6c27f6b40f5c808dd377d14b29311ed9f3
PriyaBihani <priyabihani.official@gmail.com>
2022-03-29 18:31:18 +0530

    http server

--- app/app.js
+++ app/app.js
@@ -1,34 +1,9 @@
-const fs = require('fs')
-const path = require('path')
-
-const content = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hello world</h1>
-</body>
-</html>
-`
+const http = require('http')
 
 
-// fs.renameSync(
-//     path.join(__dirname, 'welcome.js'),
-//     path.join(__dirname, 'welcome.html'))
-
-
-fs.readFile(path.join(__dirname, 'welcome.html'),{encoding:'utf8'},(err,data)=>{
-    console.log(data)
+const server = http.createServer((req,res)=>{
+    res.write('hi from the server')
+    res.end()
 })
 
-console.log('hi')
-
-
-fs.rm(path.join(__dirname, 'welcome.html'),(err)=>{
-    console.log(err)
-})
+server.listen(8080)
