36536234a9bd2d25e62e76597c4775d877067688
kartik18g <gupta.kartik18kg@gmail.com>
2022-04-07 19:16:36 +0530

    token with jwt

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,8 @@ const express = require('express')
 const jwt = require('jsonwebtoken')
 const router = express.Router()
 
+const SECRET = 'TOp_Secret_code'
+
 
 /*
 TYPE: POST
@@ -17,8 +19,48 @@ router.post('/',async(req,res)=>{
         const {email,password} = req.body
         console.log({ email, password })
 
+        const token = jwt.sign({
+            email
+        },SECRET,{})
+
         res.json({
-            todos,
+            token,
+            message: "token created successfully",
+            success: true
+        })
+
+    } catch (error) {
+        console.log(error)
+        return res.json({
+            token: null,
+            message: error.message,
+            success: false
+        })
+    }
+})
+
+
+/*
+TYPE: GET
+DESCRIPTION: Route to VERIFY YOUR TOKEN 
+BODY:null
+params:token
+*/
+
+router.get('/:token', async (req, res) => {
+    try {
+        const { token } = req.params
+
+        const isVerified = jwt.verify(token,SECRET)
+
+        if (isVerified) return res.json({
+            status:"Token verified YAY",
+            message: "token created successfully",
+            success: true
+        })
+
+        return res.json({
+            status: "Invalid token NAY",
             message: "token created successfully",
             success: true
         })
