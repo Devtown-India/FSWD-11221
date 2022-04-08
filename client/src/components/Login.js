b9d95e15c43611256414621b0e3cc15d29802982
kartik18g <gupta.kartik18kg@gmail.com>
2022-04-08 18:35:24 +0530

    change

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,15 +1,35 @@
+import axios from "axios"
+import { useState } from "react"
+
+
+
 const Login = () => {
+
+    const [email,setEmail] = useState('')
+    const [password,setPassword] = useState('')
+
+    const login = async (e)=>{
+        e.preventDefault()
+        try {
+            const { data: { token, success }} = await axios.post('http://localhost:8080/auth',{email,password})
+            console.log(token)
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+
     return ( 
         <div>
             <h1>Login to continue</h1>
-           <div>
+           <form onSubmit={login} >
                <label htmlFor="">Email</label>
-                <input type="email" />
+                <input onChange={e=>setEmail(e.target.value)} type="email" />
                 <br />
                 <label htmlFor="">Password</label>
-                <input type="password" />
-           </div>
-            <button>Login</button>
+                <input onChange={e => setPassword(e.target.value)}  type="password" />
+                <br />
+                <input type="submit" value="Login" />
+           </form>
         </div>
      );
 }
